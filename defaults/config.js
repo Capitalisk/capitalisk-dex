3b40b44f001c5ceeed372a1ee795371634142138
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2019-09-07 23:27:25 +0200

    v1.1.1

--- defaults/config.js
+++ defaults/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   requiredConfirmations: 101,
-  signatureBroadcastDelay: 22000,
+  signatureBroadcastDelay: 15000,
   orderBookSnapshotFinality: 101,
   orderBookSnapshotFilePath: './lisk-dex-orderbook-snapshot.json',
   baseChain: 'lsk',
